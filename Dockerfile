f3619a50596804e4e40895ecf9d8fa5e50628355
Dmitriy Bondarenko <bondarenko.d.89@gmail.com>
2020-02-10 17:25:41 +0200

    some change

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ ENV CASC_JENKINS_CONFIG=/var/jenkins_home/casc_configs/jenkins.yaml
 COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
 COPY jenkins.yaml /var/jenkins_home/casc_configs/jenkins.yaml
 
-COPY property1.txt /var/jenkins_home/property1.txt
+COPY property1.txt /usr/share/jenkins/ref/property1.txt
+COPY property2.txt /usr/share/jenkins/ref/property2.txt
 
 RUN /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/plugins.txt
---
+++ test.groovy
@@ -0,0 +1,50 @@
+#!/bin/env groovy
+
+node {
+    checkout scm
+
+    println "Get first property!!!"
+    def props1 = new Properties()
+    def stream = new FileInputStream("/usr/share/jenkins/ref/property1.txt")
+    try {
+    props1.load(stream)
+    } finally {
+    stream.close()
+    }
+    def var1 = Arrays.asList(props1)
+    System.out.println(var1)
+    return var1
+
+    println "Get second property!!!"
+    def props2 = new Properties()
+    def stream = new FileInputStream("/usr/share/jenkins/ref/property2.txt")
+    try {
+    props2.load(stream)
+    } finally {
+    stream.close()
+    }
+    def var2 = Arrays.asList(props2)
+    System.out.println(var2)
+    return var2
+
+
+properties([buildDiscarder(logRotator(numToKeepStr: '100')),
+    parameters([
+        [$class: 'ChoiceParameter', choiceType: 'PT_SINGLE_SELECT', description: 'filename', filterLength: 1, filterable: false, name: 'files', 
+        script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: false, script: ''], 
+        script: [classpath: [], sandbox: false,
+        script: sh(returnStdout:true, script: 'cat /usr/share/jenkins/ref/property1.txt')]]], 
+        [$class: 'CascadeChoiceParameter', choiceType: 'PT_SINGLE_SELECT', description: '', filterLength: 1, filterable: false, name: 'value', randomName: 'choice-parameter-941595264446171', referencedParameters: 'files', 
+        script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: false, script: ''], 
+        script: [classpath: [], sandbox: false, script: sh(returnStdout:true, script: 'cat /usr/share/jenkins/ref/property2.txt')]]]])])
+
+/*properties([
+    parameters([
+        [$class: 'ChoiceParameter', choiceType: 'PT_SINGLE_SELECT', description: 'filename', filterLength: 1, filterable: false, name: 'files', randomName: 'choice-parameter-941595250365512', 
+        script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: false, script: ''], 
+        script: [classpath: [], sandbox: false, 
+        script: sh(returnStdout:true, script: 'cat /var/jenkins_home/workspace/trap1703/res1.txt')]]], 
+        [$class: 'CascadeChoiceParameter', choiceType: 'PT_SINGLE_SELECT', description: '', filterLength: 1, filterable: false, name: 'value', randomName: 'choice-parameter-941595264446171', referencedParameters: 'files', 
+        script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: false, script: ''], 
+        script: [classpath: [], sandbox: false, script: sh(returnStdout:true, script: 'cat /var/jenkins_home/workspace/trap1703/res2.txt')]]]])])
+*/
